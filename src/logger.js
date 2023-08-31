5238a80b959f6bee2c9f5164740ff8d9ffd28dad
ChillerDragon <chillerdragon@gmail.com>
2023-08-31 11:34:14 +0200

    Teeworlds styled timestamp in logs
    Generate a new timestamp on restart.

--- src/logger.js
+++ src/logger.js
@@ -2,11 +2,18 @@
 
 const fs = require('fs')
 
+let _logfile = null
+
 const log = (type, msg) => {
   const ts = new Date().toISOString().split('T').join(' ').split(':').join(':').split('.')[0]
   const logmsg = `[${ts}][${type}] ${msg}`
   console.log(logmsg)
-  fs.appendFile('logs/logfile.txt', logmsg + '\n', (err) => {
+  if (!_logfile) {
+    // 2023-08-31 09:30:15 -> 2023-08-31_09-30
+    const ts_slug = ts.replace(' ', '_').replace(':', '-').split(':')[0]
+    _logfile = `logs/chillerbot_api_${ts_slug}.txt`
+  }
+  fs.appendFile(_logfile, logmsg + '\n', (err) => {
     if (err) {
       throw err
     }
